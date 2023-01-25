743dd73803e81e1f49667912fc05ec4e6e65f123
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-01-25 17:32:58 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,36 @@
+export const item = {
+    "id": "af7f86d4-5127-4a05-aaba-6c934daf8722",
+    "jumbo_title": "Agent range my director garden begin old they.",
+    "jumbo_list": [
+        {
+            "id": "b3ce9157-3585-46e1-8006-57148c48d93e",
+            "jumbo_background_img": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/67.png",
+            "jumbo_advice": "Together while organization. Question now why money page.",
+            "about_jumbo": "Truth political position song mention environment. Process people interest glass. Myself southern phone box need."
+        },
+        {
+            "id": "6bd8406f-c15d-432a-8193-7db1093c350f",
+            "jumbo_background_img": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/185.png",
+            "jumbo_advice": "College subject serious suddenly study go possible accept. Pull draw truth water including certainly rather.",
+            "about_jumbo": "Significant painting board whose. Early one TV health can edge. Among even production recently morning husband wonder she."
+        },
+        {
+            "id": "4f6d2912-e300-4577-b38b-8481cce996b8",
+            "jumbo_background_img": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/263.png",
+            "jumbo_advice": "Rise exist nearly game serve. Vote money throw soon.",
+            "about_jumbo": "Lot conference worry benefit either strategy clearly lay. Tv study such management time."
+        },
+        {
+            "id": "e58c381e-6157-400e-b3f3-2e7487f1db7b",
+            "jumbo_background_img": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/92.png",
+            "jumbo_advice": "Cup matter student star million teach. Fight church individual. Court wonder part explain.",
+            "about_jumbo": "Rate short economy fear deal what own plan."
+        },
+        {
+            "id": "bea627b4-45ff-4d89-b034-41287f8cb601",
+            "jumbo_background_img": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/233.png",
+            "jumbo_advice": "Current deal if. Accept eight remember ability already leave food. Per about factor road pass far.",
+            "about_jumbo": "Point case politics girl town ball of. Week building majority name."
+        }
+    ]
+}
